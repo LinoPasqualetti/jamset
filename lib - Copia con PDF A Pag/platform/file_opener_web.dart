45a46874973717647ba97e314457239060dc1128
Lino Pasqualetti <linopasqualetti@libero.it>
2025-11-19 11:35:38 +0100

    2025-11-19 fix(pdf): Unificata la logica di apertura dei PDF tra DB e CSV

--- lib - Copia con PDF A Pag/platform/file_opener_web.dart
+++
@@ -1,9 +0,0 @@
-// File: lib/file_opener_web.dart
-import 'package:flutter/material.dart';
-
-Future<void> openPdfPlatformSpecific({required BuildContext context,
-  required String filePath,
-  required String pageNumber,
-}) async {
-  print("Tentativo di apertura Web (attualmente non implementato).");
-}
